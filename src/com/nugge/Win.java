70fc4d71ecbc905aa423cc3ad7a99b509954e7f7
Nugge Boman <nuggex@gmail.com>
2019-10-09 20:02:37 +0300

    Added Straight flush and royal straight flush
    Royal straight flush broke some shit again...

--- src/com/nugge/Win.java
+++ src/com/nugge/Win.java
@@ -26,7 +26,7 @@ public class Win {
 
         // this is for finding pairs, triples or quads //
 
-        for(int i = 0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             lista[kortvarden[i]]++;
         }
 
@@ -38,6 +38,11 @@ public class Win {
         Arrays.sort(lista);
         int x = lista[lista.length - 1];
         int y = lista[lista.length - 2];
+        boolean flush = false;
+        boolean straight = false;
+        boolean king = false;
+
+        if (kortvarden[4] == 13) king = true;
         if (x == 4) {
             System.out.println("You have for of a kind!! ");
             prize = 12;
@@ -60,13 +65,32 @@ public class Win {
             prize = 2;
         }
         if (kortsuits[0].equals(kortsuits[1]) && kortsuits[1].equals(kortsuits[2]) && kortsuits[2].equals(kortsuits[3]) && kortsuits[3].equals(kortsuits[4])) {
-            System.out.println("Du har en flush");
-            prize = 12;
+            //System.out.println("Du har en flush");
+            flush = true;
         }
         if (kortvarden[4] - kortvarden[3] == 1 && kortvarden[3] - kortvarden[2] == 1 && kortvarden[2] - kortvarden[1] == 1 && kortvarden[1] - kortvarden[0] == 1) {
-            System.out.println("Du har en straight!!");
+            straight = true;
+        }
+        if (kortvarden[4] - kortvarden[3] == -12 && kortvarden[3] - kortvarden[2] == 1 && kortvarden[2] - kortvarden[1] == 1 && kortvarden[1] - kortvarden[0] == 1) {
+            straight = true;
+        }
+        if (flush == true && straight == false) {
+            System.out.println("You have a flush ");
             prize = 12;
         }
+        if (flush == false && straight == true) {
+            System.out.println("You have a straight");
+            prize = 10;
+        }
+        if (flush == true && straight == true) {
+            System.out.println("You have a straight flush");
+            prize = 50;
+        }
+        if (flush == true && straight == true && king == true){
+            System.out.println("You have a Royal Straight Flush");
+            prize = 100;
+        }
+
         return prize;
 
 
